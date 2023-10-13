9fe19f161b281dca83c1a7e2955668ce75d41b0d
Vineeth <saivineethkumar@gmail.com>
2023-10-13 11:06:32 -0400

    Adding skillbar and introbar sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import NavBar from "./components/NavBar/navbar"
+import Intro from "./components/Intro/Intro"
+import Skills from "./components/Skills/skills"
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
+      <Intro/>
+      <Skills />
     </div>
   )
 }
