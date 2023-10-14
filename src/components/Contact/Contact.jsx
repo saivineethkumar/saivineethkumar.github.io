791ee1b79f5afb75eaee4d99921ba84e466caf14
Vineeth <saivineethkumar@gmail.com>
2023-10-14 00:16:15 -0400

    Adding Emailjs to get emails after submitting forms

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import FacebookIcon from "../../assets/facebook-icon.png"
 import TwitterIcon from "../../assets/twitter.png"
 import InstagramIcon from "../../assets/instagram.png"
 import YoutubeIcon from "../../assets/youtube.png"
 import "./contact.css"
+import emailjs from '@emailjs/browser';
+
+const SERVICE_ID = "service_ronge0s"
+const TEMPLATE_ID = "template_hmz8i7h"
+const PUBLIC_KEY = "5sVjILnw5w-JP2wTl"
 
 export default function Contact(){
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+          .then((result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert('Email sent. Thank you!')
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
     return (
         <section id="contactPage">
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className="contactDesc">Please reachout me by using the form below.</span>
-                <form className="contactForm">
-                    <input type="text" className="name" placeholder="Your Name" />
-                    <input type="email" className="email" placeholder="Your Email" />
-                    <textarea className="msg" name="message" row="5" placeholder="Your Message"></textarea>
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder="Your Name" name='from_name' />
+                    <input type="email" className="email" placeholder="Your Email" name="from_email"/>
+                    <textarea className="msg" row="5" placeholder="Your Message" name="message" ></textarea>
                     <button type="submit" value="send" className="submitBtn">Submit</button>
                     <div className="links">
                         <img src={FacebookIcon} alt="Facebook" className="link" />
