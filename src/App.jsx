4ab193a277dbefb59d13acab23081ff0d2c67807
Vineeth <saivineethkumar@gmail.com>
2023-10-16 13:37:04 -0400

    Setting up css variables and basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import NavBar from "./components_old/NavBar/navbar"
-import Intro from "./components_old/Intro/Intro"
-import Skills from "./components_old/Skills/skills"
-import Works from "./components_old/Works/Works"
-import Contact from "./components_old/Contact/Contact"
-import Footer from "./components_old/Footer/Footer"
+import styles from "./App.module.css"
 
 export default function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Intro/>
-      <Skills />
-      <Works />
-      <Contact />
-      <Footer />
+    <div className="{styles.App}">
+      Hello World
     </div>
   )
 }
