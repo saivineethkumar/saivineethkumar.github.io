6a16d1f7e44b4e0223b559c33f3924262d4c9d56
Vineeth <saivineethkumar@gmail.com>
2023-10-17 15:58:24 -0400

    Adding projects section and its css

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from "./Projects.module.css"
+import projects from "../../data/projects.json"
+import { ProjectCard } from './ProjectCard'
+
+const Projects = () => {
+  return (
+    <section className={styles.container}>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projects}>
+            {
+                projects.map((project, id) => {
+                    return <ProjectCard key={id} project={project} />
+                })
+            }
+        </div>
+        
+    </section>
+  )
+}
+
+export default Projects
