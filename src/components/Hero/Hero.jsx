3ea57d9e085a2794dc98a71a1cad93a82679e772
Vineeth <saivineethkumar@gmail.com>
2023-10-19 16:34:47 -0400

    Modifying navbar and intro sections

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {getImageUrl} from "../../utils"
 
 import styles from "./Hero.module.css"
+import misc from "../../data/misc.json"
 
 const Hero = () => {
   return (
@@ -9,17 +10,17 @@ const Hero = () => {
         <div className={styles.content}>
           <span className={styles.hello}>Hello,</span>
           <span className={styles.introText}>
-            I'm <span className={styles.introName}>Vineeth</span> <br /> Software Developer{" "}
+            I'm <span className={styles.introName}>{misc.name}</span> <br /> {misc.intro_role}{" "}
           </span>
             <p className={styles.description}>
-            I am an experienced Software Developer, intrested in developing products that can change the world for better.
+            {misc.intro_summary}
             </p>
             <div className={styles.buttons}>
               <a href='#contact' className={styles.contactBtn}>Contact Me</a>
-              <a href='' className={styles.downloadBtn}>Download CV</a>
+              <a href={misc.resume_link} target="_blank" rel="noopener noreferrer" className={styles.downloadBtn}>Download CV</a>
             </div>
         </div>
-        <img src={getImageUrl("hero/heroImage.png")} alt='Image of me' className={styles.heroImg}/>
+        <img src={getImageUrl("intro/profileImage.png")} alt='Image of me' className={styles.heroImg}/>
     </section>
   )
 }
