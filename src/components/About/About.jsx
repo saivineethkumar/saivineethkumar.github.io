5c68e8f993bcab8d06689afca2dec68ca5b3d4ad
Vineeth <saivineethkumar@gmail.com>
2023-11-26 20:21:32 -0500

    Adding skills and updating about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
 import styles from "./About.module.css"
 import {getImageUrl} from "../../utils"
+import skills from "../../data/skills.json"
+import about from "../../data/about.json"
 
 const About = () => {
   return (
     <section className={styles.container} id="about">
-        <h2 className={styles.title}>About</h2>
-        <div className={styles.content}>
-            <ul className={styles.aboutItems}>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt='cursor icon' />
-                    <div className={styles.aboutItemText}>
-                        <h3>Frontend Developer</h3>
-                        <p>I'm a frontend developer with experience in building websites and optimised sites.</p>
-                    </div>
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/serverIcon.png")} alt='server icon' />
-                    <div className={styles.aboutItemText}>
-                        <h3>Backend Developer</h3>
-                        <p>I'm a backend developer with experience in building websites and optimised sites.</p>
-                    </div>
-                </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt='UI icon' />
-                    <div className={styles.aboutItemText}>
-                        <h3>UI Developer</h3>
-                        <p>I'm a UI developer with experience in building websites and optimised sites.</p>
-                    </div>
-                </li>
-            </ul>
+        <div className={styles.aboutContainer}>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.aboutContent}>
+                {about.map((para, id) => {
+                    return (
+                        <p className={styles.aboutPara} key={id}>{para}</p>
+                    )
+                })}
+            </div>
+        </div>
+        <div className={styles.skillsContainer}>
+            <h2 className={styles.title}>Skills</h2>
+            <div className={styles.skillsContent}>
+                {skills.map((skillCat, id) => {
+                    return ( skillCat.show ===1 &&
+                        <div className={styles.categoryItem}>
+                            <h3 className={styles.categoryTitle}>{`${skillCat.category}`}</h3>
+                            <div className={styles.categoryList}>
+                                {skillCat.list.map((skill, id) => {
+                                    return <button key={id} className={styles.skillItem}>{skill}</button>
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     </section>
-
   )
 }
 
